3561812c6135946ecbad8ad402629eb5b71b555a
minashi <aminah.xy@gmail.com>
2023-11-09 19:33:37 +1300

    update readme

--- tf/check_tfstate.sh
+++ tf/check_tfstate.sh
@@ -1,11 +1,11 @@
 #check rg exist or not
-az group show --name "rg-dev-tf" 1>/dev/null 2>&1
+az group show --name $1 1>/dev/null 2>&1
 if [ $? -ne 0 ]
 then
     echo "rg not exist,will create"
-    az group create -l "Australia east" -n "rg-dev-tf"
-    az storage account create --name "tfdevaction" --resource-group "rg-dev-tf" --location "Australia east"
-    az storage container create -n "devtfstate" --account-name "tfdevaction"
+    az group create -l "Australia east" -n $1
+    az storage account create --name $2 --resource-group $1 --location "Australia east"
+    az storage container create -n $3 --account-name $2
     sleep 60
 else
     echo "already exist"
