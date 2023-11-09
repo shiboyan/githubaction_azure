c3007f6026dfc64e3c4a15a85577d89b154444eb
minashi <aminah.xy@gmail.com>
2023-11-09 18:14:25 +1300

    debug

--- tf/backend.tf
+++ tf/backend.tf
@@ -17,9 +17,14 @@ terraform {
 }
 
 
+
 provider "azurerm" {
   use_oidc = true
   features {
   }
+    subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
   skip_provider_registration = true
 }
