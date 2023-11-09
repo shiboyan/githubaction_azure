62210dc7f7ad5175de01f4a566654c5d28b66990
minashi <aminah.xy@gmail.com>
2023-11-09 18:28:04 +1300

    debug

--- tf/variables.tf
+++ tf/variables.tf
@@ -2,15 +2,15 @@ variable "enviroment" {
   type = map(any)
 }
 
-variable "subscription_id" {
-  default = "8b1f443a-57cd-4945-9ef6-d5a2e18b49c1"
-}
-variable "client_id" {
-  default = "7ff2ed44-6798-4062-8d4c-9550c6e36a06"
-}
-variable "client_secret" {
-  default = "DBN8Q~H0mSx268Vf2-V3oDElOGe0pfFWqnhzca~D"
-}
-variable "tenant_id" {
-  default = "7ab9086d-212f-44ac-b40f-12a23a0dbed0"
-}
+#variable "subscription_id" {
+#  default = "8b1f443a-57cd-4945-9ef6-d5a2e18b49c1"
+#}
+#variable "client_id" {
+#  default = "7ff2ed44-6798-4062-8d4c-9550c6e36a06"
+#}
+#variable "client_secret" {
+#  default = "DBN8Q~H0mSx268Vf2-V3oDElOGe0pfFWqnhzca~D"
+#}
+#variable "tenant_id" {
+#  default = "7ab9086d-212f-44ac-b40f-12a23a0dbed0"
+#}
