b4d60093b3053bbc6744a46f0702ae54756d0feb
minashi <aminah.xy@gmail.com>
2023-11-09 17:50:48 +1300

    debug

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,3 +1,4 @@
+data "azurerm_client_config" "current" {}
 terraform {
     required_providers {
     azurerm = {
@@ -6,13 +7,14 @@ terraform {
     }
   }
   required_version = ">=1.1.7"
-#  backend "azurerm" {
-#    resource_group_name  = "rg-dev-tf"
-#    storage_account_name = "tfdevaction"
-#    container_name       = "devtfstate"
-#    key                  = "dev.tfstate"
-#  }
+  backend "azurerm" {
+    resource_group_name  = "rg-dev-tf"
+    storage_account_name = "tfdevaction"
+    container_name       = "devtfstate"
+    key                  = "dev.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   use_oidc = true
