dfded194f53dddfaa7d2c0793d8b6fbcc31e1d8b
minashi <aminah.xy@gmail.com>
2023-11-09 17:05:04 +1300

    add script

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-dev-tf"
-    storage_account_name = "tfdev"
+    storage_account_name = "tfdevaction"
     container_name       = "devtfstate"
     key                  = "dev.tfstate"
   }
