8d0aef759fb54966b8dfe70a8449072e72c1305e
minashi <aminah.xy@gmail.com>
2023-11-09 17:24:04 +1300

    debug

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,4 +1,10 @@
 terraform {
+    required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "rg-dev-tf"
     storage_account_name = "tfdevaction"
