e6af0f44842b497fff9da0837d6fe8389f7ae3aa
minashi <aminah.xy@gmail.com>
2023-11-14 21:02:56 +1300

    update backend sa name

--- tf/check_tfstate.sh
+++ tf/check_tfstate.sh
@@ -4,7 +4,7 @@ if [ $? -ne 0 ]
 then
     echo "rg not exist,will create"
     az group create -l "Australia east" -n $1
-    az storage account create --name $2 --resource-group $1 --location "Australia east"
+    az storage account create --name $2 --resource-group $1 --location "Australia east"  --sku Standard_LRS
     az storage container create -n $3 --account-name $2
     sleep 60
 else
